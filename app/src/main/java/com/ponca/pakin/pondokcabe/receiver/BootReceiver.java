a0f5f379be150bd1a23d7bf3400b42a9c0cbd4c2
Kelvin <Wong>
2019-05-11 13:46:24 +0700

    fixing reboot onReceive not running

--- app/src/main/java/com/ponca/pakin/pondokcabe/receiver/BootReceiver.java
+++ app/src/main/java/com/ponca/pakin/pondokcabe/receiver/BootReceiver.java
@@ -9,7 +9,7 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals("android.intent.BOOT_COMPLETED")){
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")){
             //Set Alarm Here
             QuoteAlarmManager quoteAlarmManager = new QuoteAlarmManager(context);
             quoteAlarmManager.initQuoteAlarmManager();
